45cf721636d125b62a2225d50861d80c7192ddd3
pablo <pablodelfantexp@gmail.com>
2020-11-14 20:38:28 -0300

    V2.6 commit de archivo traerPersonas para repositorio

---
+++ js/traePersonas.js
@@ -0,0 +1,47 @@
+export const personas = [
+    {
+        nombre: 'a',
+        genero: 'm',
+        feed: 10,
+        media: 30000,
+        orientacion: 'web'
+    },
+    {
+        nombre: 'b',
+        genero: 'm',
+        feed: 90,
+        media: 50000,
+        orientacion: 'diseño'
+    },
+    {
+        nombre: 'c',
+        genero: 'f',
+        feed: 80,
+        media: 40000,
+        orientacion: 'app'
+    }
+];
+
+/* personas = [
+    {
+        nombre: 'a',
+        genero: 'm',
+        feed: 10,
+        media: 30000,
+        orientacion: 'web'
+    },
+    {
+        nombre: 'b',
+        genero: 'm',
+        feed: 90,
+        media: 50000,
+        orientacion: 'diseño'
+    },
+    {
+        nombre: 'c',
+        genero: 'f',
+        feed: 80,
+        media: 40000,
+        orientacion: 'app'
+    }
+]; */
