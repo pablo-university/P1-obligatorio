8db1cc3e04e2f8be90d25d5b12a276eee55a5335
pablodelfante PC <pablodelfantexp@gmail.com>
2020-12-09 18:16:44 -0300

    V4.2 comentarios varios, un poco de orden en codigo de app

--- serverLocal.js
+++ serverLocal.js
@@ -1,3 +1,4 @@
+// --- REQUERIMIENTOS ---
 // fs
 var fs = require('fs');
 // express
@@ -5,22 +6,31 @@ var express = require('express');
 // cors
 var cors = require('cors');
 // path
-const path = require('path');
+const path = require('path');// sin usar
 
 
-// crea app expres o server
+// @crea app expres o server
 var app = express();
 
 /* 
---- MIDELWARES ---
-(son como pre procesadores del request, ej json() convierte
-la petición en json para ser manejada, está morgan xra saber donde paso el usuario,
+--- MIDDELWARES ---
+(son como pre procesadores del request response y tienen next()(xra el siguiente middleware),
+ejemplo el json() convierte
+la petición en json para ser manejada, está morgan (paquete) xra saber donde paso/intento ingresar el usuario,
 algunos de ellos vienen con expres, otros deben ser instalados, podríamos hasta
-crearlos nosotros mismos) */
+crearlos nosotros mismos ya que son funciones!) */
 // usa midelware cors
 app.use(cors())
-// MIDELWARE: para tener archivos estaticos en el fronted (imagenes css js etc)
+// MIDDELWARE: para tener archivos estaticos en el fronted (imagenes css js etc)
 app.use(express.static('public'))
+
+// - ejemplo de middleware -
+var nombreMiddleware = function (req, res, next) {
+  // codigo de lo que vaya a hacer
+  next();// da paso al siguiente middleware del codigo
+};
+app.use(nombreMiddleware); // asi ejecuto los middlewares!
+// ----------------
 
 
 // declaro personas
@@ -43,7 +53,7 @@ app.get('/guardar', function (req, res, next) {
   // aqui respondo segun me pidas
   res.send('Mi Server => archivo guardado en local');
   res.end();
-})
+})// fin de /guardar
 
 // --- ESCUCHANDO PUERTO ---
 app.listen(8080, function () {
